cdc2399720ed5b5d9f1b5846fed8b35b0d53744d
Natdanai Trintawat <natdanaitrintawat@gmail.com>
2024-07-30 10:13:50 +0700

    plot scatter image

--- plot.py
+++ plot.py
@@ -6,9 +6,11 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+IMAGE_SIZE = 20
+
 def getImage(path, zoom=1):
     img = Image.open(path)
-    img = img.resize((15, 15), Image.Resampling.LANCZOS)  # Resize the image to 10x10 pixels
+    img = img.resize((IMAGE_SIZE, IMAGE_SIZE), Image.Resampling.LANCZOS)  # Resize the image to 10x10 pixels
     return OffsetImage(np.array(img), zoom=zoom)
 
 
@@ -17,11 +19,14 @@ paths = df.path.values
 x = df.x.values
 y = df.y.values
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(20,20))
 ax.scatter(x, y) 
 
 for x0, y0, path in zip(x, y, paths):
     ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False)
     ax.add_artist(ab)
 
+plt.title('TSNE Visualization of Face Recognition Using ArcFace')
+plt.xlabel('TSNE Dimension 1')
+plt.ylabel('TSNE Dimension 2')
 plt.show()
